36ad658332e9b18a7c04568939f243b72aa2a2fa
Sang <106218734+sang9984@users.noreply.github.com>
2023-09-05 12:21:26 +0900

    Time: 3 ms (64.41%), Space: 13.3 MB (86.47%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.swift
+++ 0509-fibonacci-number/0509-fibonacci-number.swift
@@ -1,7 +1,6 @@
 class Solution {
     func fib(_ n: Int) -> Int {
-        if n == 0 {return 0}
-        if n == 1 {return 1}
+        if n < 2 {return n}
         var nums = [0,1]  
 
         for i in 2...n {
