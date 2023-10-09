dcb7d9122544e26c2b0d76ef331d1c6be041098b
Sang <106218734+sang9984@users.noreply.github.com>
2023-10-09 13:23:06 +0900

    Time: 43 ms (85.55%), Space: 14.9 MB (6.08%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.swift
+++ 0006-zigzag-conversion/0006-zigzag-conversion.swift
@@ -1,37 +1,16 @@
 class Solution {
     func convert(_ s: String, _ numRows: Int) -> String {
-        if numRows == 1 {return s}
-        var hash = [Int:String]()
-        var result = ""
-        for i in 1...numRows {
-            hash[i] = ""
+        if numRows == 1 { return s }
+        var rows = Array(repeating: "", count: min(numRows, s.count))
+        var currentRow = 0
+        var goingDown = false
+        
+        for char in s {
+            rows[currentRow] += String(char)
+            if currentRow == 0 || currentRow == numRows - 1 { goingDown.toggle() }
+            currentRow += goingDown ? 1 : -1
         }
-
-        var down = true
-        var rowChecker = 1
-        for c in s.map{String($0)} {
-            if down {
-                hash[rowChecker]! += c
-                rowChecker += 1
-                
-                if rowChecker == numRows {
-                    down = false
-                }
-                
-            }else if !down {
-                hash[rowChecker]! += c
-                rowChecker -= 1
-
-                if rowChecker == 1 {
-                    down = true
-                }
-            }
-        }
-
-        for (i,j) in hash.sorted(by: {$0.key < $1.key}) {
-            result += j
-        }
-
-        return result
+        
+        return rows.joined()
     }
 }
