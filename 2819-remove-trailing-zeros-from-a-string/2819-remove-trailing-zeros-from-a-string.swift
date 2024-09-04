1a198fca679883ad005bd87d8e8d9c302a83c8c1
Sang <106218734+sang9984@users.noreply.github.com>
2024-09-04 09:37:57 +0900

    Time: 33 ms (67.74%), Space: 16.5 MB (77.42%) - LeetHub

--- 2819-remove-trailing-zeros-from-a-string/2819-remove-trailing-zeros-from-a-string.swift
+++ 2819-remove-trailing-zeros-from-a-string/2819-remove-trailing-zeros-from-a-string.swift
@@ -1,15 +1,10 @@
 class Solution {
-    func removeTrailingZeros(_ num: String) -> String {
-        var stack = Array(num)
+func removeTrailingZeros(_ num: String) -> String {
+  var num = num
+  while num.last == "0" {
+    num.removeLast()
+  }
+  return num
+}
 
-        for i in 0..<num.count {
-            if stack.last! == "0" {
-                stack.removeLast()
-            }else {
-                break
-            }
-        }
-
-        return String(stack)
-    }
 }
